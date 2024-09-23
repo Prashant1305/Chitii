330fb95e54fd563810885985ec2538536494a78b
prashantSingh <prashantsing13052000@gmail.com>
2024-09-23 12:49:13 +0530

    clusteraisation done

--- server/app.js
+++ server/app.js
@@ -14,85 +14,87 @@ const bodyParser = require('body-parser');
 const { socketAuthenticator } = require("./middleware/auth_middleware");
 const { initializeSocket } = require("./utils/socketSetup");
 const { initializeRedis } = require("./utils/redis/connectToRedis");
+const cluster = require("cluster");
+const numCPUs = require('node:os').availableParallelism();
+const process = require("process");
 
 const PORT = process.env.PORT || 3012;
 const app = express();
 
-const allowedOrigins = [
-    'http://www.example.com',
-    `${process.env.CORS_ORIGIN}`,
-    'https://jq4m0xhj-3000.inc1.devtunnels.ms',
-    'https://chitii.vercel.app',
-    'https://chitii.netlify.app/',
-    'http://localhost:3001'
-]
 
-var corsOptions = {
-    origin: function (origin, callback) {
-        console.log("req is from ", origin);
-        if (allowedOrigins.includes(origin) || !origin) {
-            callback(null, true); // Allow the request
-        } else {
-            callback(new Error(`Not allowed by CORS ${origin}`)); // Block the request
-        }
-    },
-    methods: "GET, POST, DELETE, PATCH, HEAD, PUT",
-    credentials: true,
-};
-app.use(cors(corsOptions));
-app.use(express.json()); // to parse incoming requests with json payload and storing it in req.body
-app.use(bodyParser.urlencoded({ extended: true })); // to parse incoming urlencoded data that contains only file
-app.use(cookieParser());
 
-// SOCKET
-const server = createServer(app);
-const io = initializeSocket(server, corsOptions)
+if (cluster.isPrimary) {
+    console.log(`Primary ${process.pid} is running`);
+    for (let i = 0; i < Math.min(3, numCPUs); i++) {
+        cluster.fork();
+    }
+}
+else {
+    console.log(`Child ${process.pid} started`);
 
-app.set('socketio', io); // Store io instance in app
+    const allowedOrigins = [
+        'http://www.example.com',
+        `${process.env.CORS_ORIGIN}`,
+        'https://jq4m0xhj-3000.inc1.devtunnels.ms',
+        'https://chitii.vercel.app',
+        'https://chitii.netlify.app/',
+        'http://localhost:3001'
+    ]
 
-io.use((socket, next) => {
-    cookieParser()(socket.request, socket.request.res, async (err) => { // cookieParser returns a middleware function which can be used like this
-        return await socketAuthenticator(err, socket, next);
+    var corsOptions = {
+        origin: function (origin, callback) {
+            console.log("req is from ", origin);
+            if (allowedOrigins.includes(origin) || !origin) {
+                callback(null, true); // Allow the request
+            } else {
+                callback(new Error(`Not allowed by CORS ${origin}`)); // Block the request
+            }
+        },
+        methods: "GET, POST, DELETE, PATCH, HEAD, PUT",
+        credentials: true,
+    };
+    app.use(cors(corsOptions));
+    app.use(express.json()); // to parse incoming requests with json payload and storing it in req.body
+    app.use(bodyParser.urlencoded({ extended: true })); // to parse incoming urlencoded data that contains only file
+    app.use(cookieParser());
+
+    // SOCKET
+    const server = createServer(app);
+    const io = initializeSocket(server, corsOptions)
+
+    app.set('socketio', io); // Store io instance in app
+
+    io.use((socket, next) => {
+        cookieParser()(socket.request, socket.request.res, async (err) => { // cookieParser returns a middleware function which can be used like this
+            return await socketAuthenticator(err, socket, next);
+        });
     });
-});
 
-// checking connection
-app.get('/check', (req, res) => {
-    res.status(200).json({
-        msg: {
-            "todos": [
-                {
-                    "id": 1,
-                    "title": "Learn React",
-                    "completed": true
-                },
-                {
-                    "id": 2,
-                    "title": "book return ticket",
-                    "completed": false
-                }
-            ]
-        }
+    // checking connection
+    app.get('/check', (req, res) => {
+        res.status(200).json({
+            msg: `hello from ${process.pid}`
+        });
     });
-});
 
-// initialize Redis
-initializeRedis();
+    // initialize Redis
+    initializeRedis();
 
-app.use('/api/auth', auth_routes);
-app.use('/api/chat', chat_routes);
-app.use('/api/user', user_routes);
-app.use('/api/admin', admin_routes);
-app.use('/api/call', call_routes);
+    app.use('/api/auth', auth_routes);
+    app.use('/api/chat', chat_routes);
+    app.use('/api/user', user_routes);
+    app.use('/api/admin', admin_routes);
+    app.use('/api/call', call_routes);
 
-// error middleware
-app.use(errorMiddleware);
+    // error middleware
+    app.use(errorMiddleware);
 
-// app.listen(PORT, () => {
-//     connectDb();
-//     console.log(`server running on port ${PORT}`);
-// })
-server.listen(PORT, () => {
-    connectDb();
-    console.log(`server running on port ${PORT}`);
-})
+    // app.listen(PORT, () => {
+    //     connectDb();
+    //     console.log(`server running on port ${PORT}`);
+    // })
+    server.listen(PORT, () => {
+        connectDb();
+        console.log(`server running on port ${PORT}`);
+    })
+}            
